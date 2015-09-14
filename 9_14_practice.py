34e249a3b6e93f80ad997dbca265b035950c397c
Arthur Perrin <arthurperrin@Alveyworld-20.local>
2015-09-14 10:15:49 -0600

    practice for september 14th

---
+++ 9_14_practice.py
@@ -0,0 +1,10 @@
+a = str(raw_input("Do you think this class is boring? : "))
+
+if a == "banana":
+	print "this class isn't banana"
+elif a == "yes":
+	print "nuh uh meany this class rules"
+elif a == "no":
+	print "i know this class rules, right"
+else:
+	print "what does that even mean"
